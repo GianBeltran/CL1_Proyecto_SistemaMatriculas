a4b82bb1c016b1f5a8ec2298cff2d0bda4b12e8a
Airthon1234 <131608997+Airthon1234@users.noreply.github.com>
2023-09-29 20:00:00 -0500

    Add files via upload

---
+++ test/java/pe/edu/cibertec/proyectomatriculas/ProyectoMatriculasApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.proyectomatriculas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoMatriculasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
