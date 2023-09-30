dd642682ed475bb3b0c62e9ba3891bcb695ecc87
y4zper <78707317+y4zper@users.noreply.github.com>
2023-09-29 19:46:59 -0500

    Add files via upload

---
+++ src/main/java/pe/edu/cibertec/cl1_matriculas/model/request/EstudianteRequest.java
@@ -0,0 +1,16 @@
+package pe.edu.cibertec.proyectomatriculas.model.request;
+
+import lombok.Data;
+import java.util.Date;
+
+@Data
+public class EstudianteRequest {
+    private Integer idestudiante;
+    private String nomestudiante;
+    private String apeestudiante;
+    private Date fechanac;
+    private String direccion;
+    private String correo;
+    private String telefono;
+    private String grado;
+}
