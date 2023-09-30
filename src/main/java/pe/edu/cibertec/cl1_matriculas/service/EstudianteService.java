a42675b4acdbee4121872ace325f4eab511b1bf9
y4zper <78707317+y4zper@users.noreply.github.com>
2023-09-29 19:48:12 -0500

    EstudianteService

---
+++ src/main/java/pe/edu/cibertec/cl1_matriculas/service/EstudianteService.java
@@ -0,0 +1,48 @@
+package pe.edu.cibertec.proyectomatriculas.service;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import pe.edu.cibertec.proyectomatriculas.model.bd.Estudiante;
+import pe.edu.cibertec.proyectomatriculas.model.request.EstudianteRequest;
+import pe.edu.cibertec.proyectomatriculas.model.response.ResultadoResponse;
+import pe.edu.cibertec.proyectomatriculas.repository.EstudianteRepository;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class EstudianteService {
+
+    private EstudianteRepository estudianteRepository;
+
+    public List<Estudiante> listarEstudiantes(){
+        return estudianteRepository.findAll();
+    }
+
+    public ResultadoResponse guardarEstudiante(EstudianteRequest estudiante){
+        String mensaje = "Estudiante registrado correctamente";
+        Boolean respuesta = true;
+        try {
+            Estudiante objEstudiante = new Estudiante();
+            if(estudiante.getIdestudiante() > 0){
+                objEstudiante.setIdestudiante(estudiante.getIdestudiante());
+            }
+            objEstudiante.setNomestudiante(estudiante.getNomestudiante());
+            objEstudiante.setApeestudiante(estudiante.getApeestudiante());
+            objEstudiante.setFechanac(estudiante.getFechanac());
+            objEstudiante.setDireccion(estudiante.getDireccion());
+            objEstudiante.setCorreo(estudiante.getCorreo());
+            objEstudiante.setTelefono(estudiante.getTelefono());
+            objEstudiante.setGrado(estudiante.getGrado());
+
+            estudianteRepository.save(objEstudiante);
+        } catch (Exception e){
+            mensaje = "Estudiante no registrado";
+            respuesta = false;
+        }
+        return ResultadoResponse.builder()
+                .mensaje(mensaje)
+                .respuesta(respuesta)
+                .build();
+    }
+}
