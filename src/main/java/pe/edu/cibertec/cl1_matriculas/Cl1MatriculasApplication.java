852a98d08ea0dedbe01c450a02e2aa8f39396797
GianBeltran <Gbeltranl0506@gmail.com>
2023-09-29 19:33:07 -0500

    Creacion de proyecto

---
+++ src/main/java/pe/edu/cibertec/cl1_matriculas/Cl1MatriculasApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.cl1_matriculas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cl1MatriculasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cl1MatriculasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/cl1_matriculas/Cl1MatriculasApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.cl1_matriculas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cl1MatriculasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
