4a74428f44ad3da4d30cd4fc8b0b3a64ae4dfbeb
y4zper <78707317+y4zper@users.noreply.github.com>
2023-09-29 19:46:35 -0500

    Add files via upload

---
+++ src/main/java/pe/edu/cibertec/cl1_matriculas/model/bd/Estudiante.java
@@ -0,0 +1,34 @@
+package pe.edu.cibertec.proyectomatriculas.model.bd;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "estudiantes")
+@Getter @Setter @NoArgsConstructor
+public class Estudiante {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idestudiante;
+
+    @Column(name = "nomestudiante")
+    private String nomestudiante;
+    @Column(name = "apeestudiante")
+    private String apeestudiante;
+    @Column(name = "fechanac")
+    private Date fechanac;
+    @Column(name = "direccion")
+    private String direccion;
+    @Column(name = "correo")
+    private String correo;
+    @Column(name = "telefono")
+    private String telefono;
+    @Column(name = "grado")
+    private String grado;
+
+}
